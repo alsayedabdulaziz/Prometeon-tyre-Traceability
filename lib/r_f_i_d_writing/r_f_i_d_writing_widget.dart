db505aa465f264849da96c3c7194594c0b686c8d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-14 11:16:17 +0000

    Updating to latest FlutterFlow output.

--- lib/r_f_i_d_writing/r_f_i_d_writing_widget.dart
+++ lib/r_f_i_d_writing/r_f_i_d_writing_widget.dart
@@ -2,8 +2,10 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/instant_timer.dart';
 import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'r_f_i_d_writing_model.dart';
@@ -25,6 +27,25 @@ class _RFIDWritingWidgetState extends State<RFIDWritingWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => RFIDWritingModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await Future.wait([
+        Future(() async {
+          _model.instantTimer = InstantTimer.periodic(
+            duration: Duration(milliseconds: 500),
+            callback: (timer) async {
+              _model.scannedbarcode = valueOrDefault<String>(
+                FFAppState().ScanResult.toString(),
+                '-',
+              );
+              safeSetState(() {});
+            },
+            startImmediately: true,
+          );
+        }),
+      ]);
+    });
 
     _model.scanthebarcodeTextController ??= TextEditingController();
     _model.scanthebarcodeFocusNode ??= FocusNode();
@@ -307,7 +328,7 @@ class _RFIDWritingWidgetState extends State<RFIDWritingWidget> {
                                           0.0, 5.0, 0.0, 0.0),
                                       child: Text(
                                         valueOrDefault<String>(
-                                          FFAppState().ScanResult.toString(),
+                                          _model.scannedbarcode,
                                           '-',
                                         ),
                                         style: FlutterFlowTheme.of(context)
